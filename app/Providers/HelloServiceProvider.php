c3a0ddb4499dbd9913f34c11a00cecd6cb3eb001
moriken4320 <moriken.4320@gmail.com>
2021-01-16 19:36:12 +0900

    ビューコンポーザクラスを作成する

--- app/Providers/HelloServiceProvider.php
+++ app/Providers/HelloServiceProvider.php
@@ -25,9 +25,8 @@ class HelloServiceProvider extends ServiceProvider
     public function boot()
     {
         view()->composer(
-            'hello.index', function($view){
-                $view->with('view_message', 'composer message!');
-            }
+            'hello.index',
+            'App\Http\Composers\HelloComposer'
         );
     }
 }
